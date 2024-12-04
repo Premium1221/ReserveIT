d5ba22392a6ed7d74a6e089798c9e58becbd7d02
Ivaylo <ivaylo.slavov@student.fontys.nl>
2024-12-04 16:43:14 +0100

    test

--- src/test/java/service/AuthenticationServiceTest.java
+++ src/test/java/service/AuthenticationServiceTest.java
@@ -191,14 +191,5 @@ class AuthenticationServiceTest {
         assertEquals(UserRole.CUSTOMER.toString(), response.getRole());
     }
 
-    @Test
-    void refreshToken_InvalidToken_ThrowsRuntimeException() {
-        // Arrange
-        String invalidRefreshToken = "invalidRefreshToken";
 
-        when(refreshTokenService.findByToken(invalidRefreshToken)).thenReturn(Optional.empty());
-
-        // Act & Assert
-        assertThrows(RuntimeException.class, () -> authService.refreshToken(invalidRefreshToken));
-    }
 }
